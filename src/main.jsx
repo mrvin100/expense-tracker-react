d1e35d3c7ef4bf80893cf597dfa37ff1950853d6
vincent youmssi <vincentyoumssi@gmail.com>
2024-06-18 12:24:07 +0100

    add of complete porject to repository

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import GlobalState from "./context/index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GlobalState>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </GlobalState>
+);
